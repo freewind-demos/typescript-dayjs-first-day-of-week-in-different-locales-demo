8ad483afe4b994c9c2215f37ac566ad646f18cc3
Freewind <nowindlee@gmail.com>
2025-04-24 19:14:57 +0800

    add en-gb

--- hello.ts
+++ hello.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 
 import 'dayjs/locale/en'
+import 'dayjs/locale/en-gb'
 import 'dayjs/locale/de'
 import 'dayjs/locale/zh-cn'
 
@@ -10,6 +11,7 @@ console.log('Default locale:', dayjs.locale())
 // first day of week for different locales
 console.log(dayjs('2025-04-20').locale('zh-cn').startOf('week').format('YYYY-MM-DD'))
 console.log(dayjs('2025-04-20').locale('en').startOf('week').format('YYYY-MM-DD'))
+console.log(dayjs('2025-04-20').locale('en-gb').startOf('week').format('YYYY-MM-DD'))
 console.log(dayjs('2025-04-20').locale('de').startOf('week').format('YYYY-MM-DD'))
 
 dayjs.locale('de');
